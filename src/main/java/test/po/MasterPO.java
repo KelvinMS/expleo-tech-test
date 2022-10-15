32b91a69d1ad1f985f1decf73b309822cafe6ed9
Kelvin M <kelvin651b@gmail.com>
2022-10-15 14:05:05 -0300

    Setting timeout globally

--- src/main/java/test/po/MasterPO.java
+++ src/main/java/test/po/MasterPO.java
@@ -1,2 +1,24 @@
-package test.po;public class MasterPO {
-}
+package test.po;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class MasterPO {
+
+        final ThreadLocal<WebDriver> driver;
+        final WebDriverWait wait;
+
+    public MasterPO(ThreadLocal<WebDriver> driver){
+            this.driver = driver;
+            this.driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            PageFactory.initElements(this.driver.get(), this);
+            wait = new WebDriverWait(this.driver.get(), 5);
+        }
+
+        public WebDriverWait getWait(){
+            return wait;
+        }
+    }
